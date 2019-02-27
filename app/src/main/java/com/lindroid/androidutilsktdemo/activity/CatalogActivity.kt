e3fb3fa83dcad0275784f8f9a923dcafa2590f89
Lindroy <linyulong2014@163.com>
2019-02-27 15:47:51 +0800

    编写Toast工具类和AppUtil工具类用例。

--- app/src/main/java/com/lindroid/androidutilsktdemo/activity/CatalogActivity.kt
+++ app/src/main/java/com/lindroid/androidutilsktdemo/activity/CatalogActivity.kt
@@ -1,7 +1,10 @@
 package com.lindroid.androidutilsktdemo.activity
 
+import android.content.Intent
+import android.widget.ArrayAdapter
 import com.lindroid.androidutilsktdemo.R
 import com.lindroid.androidutilsktdemo.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_catalog.*
 
 /**
  * @author Lin
@@ -10,8 +13,32 @@ import com.lindroid.androidutilsktdemo.base.BaseActivity
  * @Description
  */
 class CatalogActivity(override val contentViewId: Int = R.layout.activity_catalog) : BaseActivity() {
+    private lateinit var map: LinkedHashMap<Int, Class<*>>
+    private val names: MutableList<String> = ArrayList()
+
+    override fun initBefore() {
+        super.initBefore()
+        map = linkedMapOf(
+                R.string.util_app_info to AppInfoActivity::class.java,
+                R.string.util_toast to ToastActivity::class.java
+        )
+        names.addAll(map.map {
+            getString(it.key)
+        })
+    }
 
     override fun initView() {
         super.initView()
+        initToolBar(title = "目录", isShowArrow = false)
+        fun startActivity(cls: Class<*>?) {
+            startActivity(Intent(mContext, cls))
+        }
+        list.adapter = ArrayAdapter<String>(mContext, android.R.layout.simple_list_item_1, android.R.id.text1, names)
+        list.setOnItemClickListener { parent, view, position, id ->
+            val rightMap = map.filterKeys {
+                getString(it) == names[position]
+            }.values.toList()
+            startActivity(rightMap[0])
+        }
     }
 }
