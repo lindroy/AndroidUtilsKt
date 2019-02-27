cf731a9ca8150afd9a500f5ef11d90d83b3f7fa9
Lindroy <linyulong2014@163.com>
2019-02-27 10:30:23 +0800

    创建工程。

---
+++ app/src/main/java/com/lindroid/androidutilsktdemo/activity/CatalogActivity.kt
@@ -0,0 +1,13 @@
+package com.lindroid.androidutilsktdemo.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.lindroid.androidutilsktdemo.R
+
+class CatalogActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_catalog)
+    }
+}
