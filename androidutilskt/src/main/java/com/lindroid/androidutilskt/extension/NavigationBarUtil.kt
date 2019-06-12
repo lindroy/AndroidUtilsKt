83f67254c9c27e36d574e09cdbb79fb7aeddda0f
Lindroy <linyulong2014@163.com>
2019-06-12 14:31:12 +0800

    测试显示和隐藏导航栏的方法。

--- androidutilskt/src/main/java/com/lindroid/androidutilskt/extension/NavigationBarUtil.kt
+++ androidutilskt/src/main/java/com/lindroid/androidutilskt/extension/NavigationBarUtil.kt
@@ -3,9 +3,12 @@
 package com.lindroid.androidutilskt.extension
 
 import android.app.Activity
+import android.os.Build
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import com.lindroid.androidutilskt.app.AndUtil
+
 
 /**
  * @author Lin
@@ -37,17 +40,77 @@ val hasNavBar
     }*/
 
 /**
- * 当前是否显示虚拟导航栏
+ * 当前虚拟导航栏是否显示
  */
 val Activity.isNavBarShowed: Boolean
+    get() = this.window.isNavBarShowed
+
+/**
+ * 当前虚拟导航栏是否显示
+ */
+val Window.isNavBarShowed: Boolean
     get() {
-        val viewGroup = window.decorView as ViewGroup? ?: return false
+        val viewGroup = decorView as ViewGroup? ?: return false
         return (0 until viewGroup.childCount).firstOrNull {
             viewGroup.getChildAt(it).id != View.NO_ID
-                    && resources.getResourceEntryName(viewGroup.getChildAt(it).id) == "navigationBarBackground"
+                    && AndUtil.appContext.resources.getResourceEntryName(viewGroup.getChildAt(it).id) == RES_NAME_NAV_BAR
         } != null
-
     }
+
+/**
+ * 当前虚拟导航栏是否隐藏
+ */
+val Window.isNavBarHidden: Boolean
+    get() = !isNavBarShowed
+
+/**
+ * 当前虚拟导航栏是否隐藏
+ */
+val Activity.isNavBarHidden: Boolean
+    get() = window.isNavBarHidden
+
+fun Window.hideNavBar() {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+        return
+    }
+    /* val viewGroup = decorView as ViewGroup? ?: return false
+     (0 until viewGroup.childCount).forEach {
+         val childId = viewGroup.getChildAt(it).id
+         if (childId != View.NO_ID){
+             val resourceEntryName = AndUtil.appContext.resources.getResourceEntryName(childId)
+         }else{
+             return
+         }
+     }*/
+    val uiOptions =
+        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+}
+
+fun Window.setShowNavBar(isShow: Boolean) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+        return
+    }
+    val viewGroup = decorView as ViewGroup? ?: return
+    for (i in (0 until viewGroup.childCount)){
+        val childId = viewGroup.getChildAt(i).id
+        if (childId != View.NO_ID){
+            val resourceEntryName = AndUtil.appContext.resources.getResourceEntryName(childId)
+            if (RES_NAME_NAV_BAR == resourceEntryName){
+                viewGroup.getChildAt(i).visibility = if (isShow) View.VISIBLE else View.INVISIBLE
+            }
+        }else{
+            break
+        }
+    }
+    val uiOptions =
+        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION  or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+    if (isShow) {
+        decorView.systemUiVisibility = decorView.systemUiVisibility and uiOptions.inv()
+    } else {
+        decorView.systemUiVisibility = decorView.systemUiVisibility or uiOptions
+    }
+}
+
 
 /**
  * 获取虚拟导航栏的高度，必须在布局绘制完成之后才能获取到正确的值（可以在onWindowFocusChanged()中调用）
@@ -60,6 +123,8 @@ val navBarHeight: Int
             getResDeminPx(resourceId)
         } else 0
     }
+
+private const val RES_NAME_NAV_BAR = "navigationBarBackground"
 
 private val navBarResId
     get() = AndUtil.appContext.resources.getIdentifier(
--- app/src/main/java/com/lindroid/androidutilsktdemo/activity/NavigationBarActivity.kt
+++ app/src/main/java/com/lindroid/androidutilsktdemo/activity/NavigationBarActivity.kt
@@ -3,6 +3,7 @@ package com.lindroid.androidutilsktdemo.activity
 import com.lindroid.androidutilskt.extension.hasNavBar
 import com.lindroid.androidutilskt.extension.isNavBarShowed
 import com.lindroid.androidutilskt.extension.navBarHeight
+import com.lindroid.androidutilskt.extension.setShowNavBar
 import com.lindroid.androidutilsktdemo.R
 import com.lindroid.androidutilsktdemo.base.BaseActivity
 import com.youngfeng.snake.annotations.EnableDragToClose
@@ -27,6 +28,14 @@ class NavigationBarActivity(override val contentViewId: Int = R.layout.activity_
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         tvNavShow.text = "虚拟导航栏是否显示：$isNavBarShowed"
+        swNav.setOnCheckedChangeListener { buttonView, isChecked ->
+            window.setShowNavBar(isChecked)
+           /* if (isChecked){
+                llRoot.setNewPadding(top = 0)
+            }else{
+                llRoot.setNewPadding(top = statusBarHeight)
 
+            }*/
+        }
     }
 }
